5ff2b40737521038f5a9b9e7ff99382c21884af0
Hakimi keerai <hakeemee222@gmail.com>
2022-08-27 07:32:46 +0700

    Passing Props

--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,8 +1,26 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { ImageBackground, StyleSheet, Text } from "react-native";
+import Forecast from "./Forcast";
 
 export default function Weather(props) {
-    return (
-    <Text>{props.zipCode}</Text>
-    );
-   }
+  const [forecastInfo, setForecastInfo] = useState({
+    main: "-",
+    description: "-",
+    temp: 0,
+  });
+  return (
+    <ImageBackground source={require("../bg.jpg")} style={styles.backdrop}>
+      <Text>Zip Code</Text>
+      <Text>{props.zipCode}</Text>
+      <Forecast {...forecastInfo} />
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  backdrop: {
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+});
